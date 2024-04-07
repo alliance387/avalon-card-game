2a0f403faaadc1ad6934228584c1c373ab87d856
Gazinur <ishkining@yandex-team.ru>
2024-04-07 15:03:14 +0300

    FRONTEND add first steps to look that server is ok

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { HMSPrebuilt } from '@100mslive/roomkit-react';
+
+export default function Home() {
+    return (
+        <div style={{ height: '100vh' }}>
+            <HMSPrebuilt roomCode="lqk-xhxq-pns" />
+        </div>
+    );
+}
