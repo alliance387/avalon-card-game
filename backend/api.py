38f8a9ac8ceb64a98c9ec3130a6528a826319b85
Gazinur <ishkining@yandex-team.ru>
2024-04-07 22:14:07 +0300

    [BACKEND-(1-3)] Make simple fastapi app for deploy

---
+++ backend/api.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get('/')
+async def read_root():
+    return { 
+        'message': 'Hello bitch'
+    }
+
+@app.get('/user_order/{room_id}')
+async def read_room_order(room_id: str):
+    return { 
+        'message': f'I dunno know what to do {room_id}'
+    }
+
+if __name__ == "__main__":
+    uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
