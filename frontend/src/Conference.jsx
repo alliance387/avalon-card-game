727a124664ca439c089672510c45e809b3d982b3
Gazinur <ishkining@yandex-team.ru>
2024-04-10 20:48:44 +0300

    [FRONTEND-8] CHange main focus by cliccking

--- frontend/src/Conference.jsx
+++ frontend/src/Conference.jsx
@@ -17,21 +17,26 @@ function Conference() {
     }
   }, [peers]);
 
+  function changeMainFocus(peer_id, my_peer_id) {
+    set_main_focus_peers(prevState => ({
+      ...prevState,
+      [my_peer_id]: peer_id
+    }));
+  }
+
+
   return (
     <div className="conference-section">
       <div className="peers-container-main">
         {peers.filter(outer_peer => outer_peer.id == main_focus_peer[peers.filter(peer => peer.isLocal)[0].id]).map(peer => (
-          <Peer key={peer.id} peer={peer} />
+          <Peer key={peer.id} peer={peer}/>
         ))}
-      {/* {peers.map((peer) => (
-<Peer key={peer.id} peer={peer} />
-))} */}
       </div>
       <div className="peers-container-siteitem">
         {
           peers.filter(peer => peer.id !=  main_focus_peer[peers.filter(peer => peer.isLocal)[0].id]).map(peer => (
-            <div>
-              <Peer key={peer.id} peer={peer} />
+            <div onClick={() => changeMainFocus(peer.id, peers.filter(peer => peer.isLocal)[0].id)}>
+              <Peer key={peer.id} peer={peer}/>
             </div>
           ))
         }
