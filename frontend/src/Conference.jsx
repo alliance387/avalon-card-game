0fb87793fb9ee8d16bcff8296e40a44798dda561
Gazinur <ishkining@yandex-team.ru>
2024-04-11 18:58:07 +0300

    [FRONTEND-9] add poll function

--- frontend/src/Conference.jsx
+++ frontend/src/Conference.jsx
@@ -6,6 +6,7 @@ function Conference() {
   const peers = useHMSStore(selectPeers);
 
   const [main_focus_peer, set_main_focus_peers] = useState({});
+  const [decision_array , set_decision_array] = useState({});
 
   useEffect(() => {
     let unfocused = peers.map(peer => peer.id).filter(peer_id => !(peer_id in main_focus_peer))[0];
@@ -13,6 +14,10 @@ function Conference() {
       set_main_focus_peers(prevState => ({
         ...prevState,
         [unfocused]: unfocused
+      }));
+      set_decision_array(prevState => ({
+        ...prevState,
+        [unfocused]: false
       }));
     }
   }, [peers]);
@@ -24,19 +29,28 @@ function Conference() {
     }));
   }
 
+  const changeDecision = (peer_id) => {
+    if (peers.filter(peer => peer.id === peer_id && peer.isLocal).length !== 0){
+      set_decision_array(prevState => ({
+        ...prevState,
+        [peer_id]: !decision_array[peer_id]
+      }));
+    }
+  }
+
 
   return (
     <div className="conference-section">
       <div className="peers-container-main">
         {peers.filter(outer_peer => outer_peer.id == main_focus_peer[peers.filter(peer => peer.isLocal)[0].id]).map(peer => (
-          <Peer key={peer.id} peer={peer}/>
+          <Peer key={peer.id} peer={peer} is_selected={decision_array[peer.id]} changeDecision={changeDecision}/>
         ))}
       </div>
       <div className="peers-container-siteitem">
         {
           peers.filter(peer => peer.id !=  main_focus_peer[peers.filter(peer => peer.isLocal)[0].id]).map(peer => (
             <div className="item" onClick={() => changeMainFocus(peer.id, peers.filter(peer => peer.isLocal)[0].id)}>
-              <Peer key={peer.id} peer={peer}/>
+              <Peer key={peer.id} peer={peer} is_selected={decision_array[peer.id]} changeDecision={changeDecision}/>
             </div>
           ))
         }
--- frontend/src/Peer.jsx
+++ frontend/src/Peer.jsx
@@ -1,11 +1,13 @@
 import { useVideo } from "@100mslive/react-sdk";
 
-function Peer({ peer }) {
+function Peer({ peer, is_selected, changeDecision }) {
   const { videoRef } = useVideo({
     trackId: peer.videoTrack
   });
   return (
     <div className="peer-container">
+      <img src={is_selected ? "gold_coin.png" : "grey_coin.png"} alt="" width="10%" onClick={() => changeDecision(peer.id)}/>
+      
       <video
         ref={videoRef}
         className={`peer-video ${peer.isLocal ? "local" : ""}`}
