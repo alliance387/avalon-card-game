49908d89a4f3d5c85a916e397aabdce855885baf
ishkining <ishkining@gmail.com>
2024-04-14 22:41:04 +0300

    [FRONTEND-11] Show Mafia logic

--- frontend/src/pages/Conference.jsx
+++ frontend/src/pages/Conference.jsx
@@ -1,9 +1,13 @@
 import { selectPeers, useHMSStore } from "@100mslive/react-sdk";
 import { useEffect, useState } from "react";
 import Peer from "./Peer";
+import {battleField, evil_dudes} from "../data/players";
 
 function Conference() {
   const peers = useHMSStore(selectPeers);
+
+  const [gameStage, setGameStage] = useState("");
+  const [filteredPeers, setFilteredPeers] = useState(peers);
 
   const [main_focus_peer, set_main_focus_peers] = useState({});
   const [decision_array , set_decision_array] = useState({});
@@ -38,25 +42,71 @@ function Conference() {
     }
   }
 
+  useEffect(() => {
+    if (gameStage === "") {
+      if (Object.values(decision_array).filter((status) => status == true).length >= 5){
+        // TODO axios to start game
+        setGameStage("Evil");
+        alert('1');
+      }
+    }
+    
+  }, [decision_array]);
+
+  useEffect(() => {
+    if (gameStage === "Evil") {
+      // Show mafia
+      console.log(filteredPeers.filter(peer => evil_dudes.includes(peer.roleName)));
+      setFilteredPeers(peers);
+      // setFilteredPeers(filteredPeers.filter(peer => evil_dudes.includes(peer.roleName)));
+      // setGameStage("Merlin");
+      // alert('2');
+    }
+    else if (gameStage === "Merlin") {
+      // Show Merlin
+      setGameStage("Percival");
+      alert('3');
+    }
+    else if (gameStage === "Percival") {
+      // Show Percival
+      setGameStage("Game");
+      alert('4');
+    }
+    else if (gameStage === "Game") {
+      // Show Game
+      alert('5');
+    }
+    
+  }, [gameStage]);
+
+  useEffect(() => {
+    setFilteredPeers(peers);
+  }, [peers]);
+  
+
 
   return (
     <div className="conference-section">
+      {filteredPeers.filter(peer => peer.isLocal).length !== 0 ? (<>
       <div className="peers-container-main">
-        {peers.filter(outer_peer => outer_peer.id == main_focus_peer[peers.filter(peer => peer.isLocal)[0].id]).map(peer => (
-          <div className="main-item">
-          <Peer key={peer.id} peer={peer} is_selected={decision_array[peer.id]} changeDecision={changeDecision}/>
-          </div>
-        ))}
-      </div>
-      <div className="peers-container-siteitem">
-        {
-          peers.filter(peer => peer.id !=  main_focus_peer[peers.filter(peer => peer.isLocal)[0].id]).map(peer => (
-            <div className="item" onClick={() => changeMainFocus(peer.id, peers.filter(peer => peer.isLocal)[0].id)}>
-              <Peer key={peer.id} peer={peer} is_selected={decision_array[peer.id]} changeDecision={changeDecision}/>
+        
+          {filteredPeers.filter(outer_peer => outer_peer.id == main_focus_peer[peers.filter(peer => peer.isLocal)[0].id]).map(peer => (
+            <div className="main-item">
+            <Peer key={peer.id} peer={peer} is_selected={decision_array[peer.id]} changeDecision={changeDecision}/> 
             </div>
-          ))
-        }
+          ))}
+        </div>
+        <div className="peers-container-siteitem">
+          {
+            filteredPeers.filter(peer => peer.id !=  main_focus_peer[peers.filter(peer => peer.isLocal)[0].id]).map(peer => (
+              <div className="item" onClick={() => changeMainFocus(peer.id, peers.filter(peer => peer.isLocal)[0].id)}>
+                <Peer key={peer.id} peer={peer} is_selected={decision_array[peer.id]} changeDecision={changeDecision}/>
+              </div>
+            ))
+          }
+         
       </div>
+      </>): <></>}
     </div>
   );
 }
