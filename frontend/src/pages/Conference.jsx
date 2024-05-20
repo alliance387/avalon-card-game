6af8f5264847d6d82bfbcbb8b18535670706162a
ishkining <ishkining@gmail.com>
2024-05-20 21:00:48 +0300

    [Frontend] Refactor focus state

--- frontend/src/pages/Conference.jsx
+++ frontend/src/pages/Conference.jsx
@@ -9,50 +9,43 @@ function Conference() {
   const [gameStage, setGameStage] = useState("");
   const [filteredPeers, setFilteredPeers] = useState(peers);
 
-  const [main_focus_peer, set_main_focus_peers] = useState({});
-  const [decision_array , set_decision_array] = useState({});
+  
+  
 
+  // Focus part
+  const [main_focus_peer, set_main_focus_peer] = useState("");
   useEffect(() => {
-    let unfocused = peers.map(peer => peer.id).filter(peer_id => !(peer_id in main_focus_peer))[0];
-    if (unfocused !== undefined){
-      set_main_focus_peers(prevState => ({
-        ...prevState,
-        [unfocused]: unfocused
-      }));
-      set_decision_array(prevState => ({
-        ...prevState,
-        [unfocused]: false
-      }));
+    if (main_focus_peer === "")
+    {
+      set_main_focus_peer(peers.filter(peer => peer.isLocal)[0].id);
+    }
+    else if(!main_focus_peer in peers){
+      set_main_focus_peer(peers[0].id);
     }
   }, [peers]);
 
-  function changeMainFocus(peer_id, my_peer_id) {
-    set_main_focus_peers(prevState => ({
-      ...prevState,
-      [my_peer_id]: peer_id
-    }));
+  function changeMainFocus(peer_id) {
+    set_main_focus_peer(peer_id);
   }
 
-  const changeDecision = (peer_id) => {
-    if (peers.filter(peer => peer.id === peer_id && peer.isLocal).length !== 0){
-      set_decision_array(prevState => ({
-        ...prevState,
-        [peer_id]: !decision_array[peer_id]
-      }));
+  // Decision part
+  const [decision, set_decision] = useState(0);
+  const changeDecision = (peer) => {
+    const next_value = decision === 0 ? 1 : 0;
+    if (peer.isLocal)
+    {
+      set_decision(next_value);
     }
   }
 
   useEffect(() => {
-    console.log(main_focus_peer);
     if (gameStage === "") {
-      if (Object.values(decision_array).filter((status) => status == true).length >= 5){
         // TODO axios to start game
         // setGameStage("Evil");
-        alert('1');
-      }
+        // alert('1');
     }
     
-  }, [decision_array]);
+  }, [decision]);
 
   useEffect(() => {
     if (gameStage === "Evil") {
@@ -98,17 +91,17 @@ function Conference() {
       {filteredPeers.filter(peer => peer.isLocal).length !== 0 ? (<>
       <div className="peers-container-main">
         
-          {filteredPeers.filter(outer_peer => outer_peer.id == main_focus_peer[peers.filter(peer => peer.isLocal)[0].id]).map(peer => (
+          {filteredPeers.filter(outer_peer => outer_peer.id === main_focus_peer).map(peer => (
             <div className="main-item">
-            <Peer key={peer.id} peer={peer} is_selected={decision_array[peer.id]} changeDecision={changeDecision}/> 
+            <Peer key={peer.id} peer={peer} is_selected={decision} changeDecision={changeDecision}/> 
             </div>
           ))}
         </div>
         <div className="peers-container-siteitem">
           {
-            filteredPeers.filter(peer => peer.id !=  main_focus_peer[peers.filter(peer => peer.isLocal)[0].id]).map(peer => (
-              <div className="item" onClick={() => changeMainFocus(peer.id, peers.filter(peer => peer.isLocal)[0].id)}>
-                <Peer key={peer.id} peer={peer} is_selected={decision_array[peer.id]} changeDecision={changeDecision}/>
+            filteredPeers.filter(peer => peer.id !== main_focus_peer).map(peer => (
+              <div className="item" onClick={() => changeMainFocus(peer.id)}>
+                <Peer key={peer.id} peer={peer} is_selected={decision} changeDecision={changeDecision}/>
               </div>
             ))
           }
--- frontend/src/pages/Peer.jsx
+++ frontend/src/pages/Peer.jsx
@@ -6,7 +6,7 @@ function Peer({ peer, is_selected, changeDecision }) {
   });
   return (
     <div className="peer-container">
-      <img class ="button-start"src={is_selected ? "/gold_coin.png" : "/grey_coin.png"} alt="" width="10%" onClick={() => changeDecision(peer.id)}/>
+      <img class ="button-start"src={is_selected ? "/gold_coin.png" : "/grey_coin.png"} alt="" width="10%" onClick={() => changeDecision(peer)}/>
       
       <img class="img-role" src="/Oberon.jpg" alt="" width="20%" />
 
